e3d2727f28f194a8a91b1ca20fd9bc18def084c6
Pratik Mota <mota.pratik@gmail.com>
2024-03-24 16:32:46 +0530

    data type benifit in typescript

---
+++ 4-data-types/app.ts
@@ -0,0 +1,15 @@
+/* Issue if not added data type
+    3+5 = 8
+    "3"+5 = 35
+*/
+function add1(num1, num2){
+    return num1+num2
+}
+var result1 = add1('3',5)
+console.log("add1: "+ result1)
+//== add data type
+function add2(num1: number, num2: number){
+    return num1+num2
+}
+var result2 = add2(3,5)
+console.log("add2: "+ result2)
